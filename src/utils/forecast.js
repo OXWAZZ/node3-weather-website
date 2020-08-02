ece139ac307c2097bdef529c2cd88fde98ccb184
OXWAZZ <muhammad.rahmahalim@gmail.com>
2020-08-02 14:48:34 +0700

    Setup for Heroku

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
             ',' +
             longitude;
 
-        request({ url, json: true }, (error, { body }) => {
+        request({ url, json: true }, (error, { body } = {}) => {
             if (error) {
                 callback('Unable to connect!', undefined);
             } else if (body.error) {
